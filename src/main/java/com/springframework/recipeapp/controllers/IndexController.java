ac8d026893c22527a13caff94ed36e84bb3b5643
Lukasz Proko <Lukasz.Marek.Prokop@gmail.com>
2020-06-11 01:11:59 +0200

    Added hardcoded Recipes

--- src/main/java/com/springframework/recipeapp/controllers/IndexController.java
+++ src/main/java/com/springframework/recipeapp/controllers/IndexController.java
@@ -1,13 +1,28 @@
 package com.springframework.recipeapp.controllers;
 
+import com.springframework.recipeapp.domain.Category;
+import com.springframework.recipeapp.domain.UnitOfMeasure;
+import com.springframework.recipeapp.repositories.CategoryRepository;
+import com.springframework.recipeapp.repositories.UnitOfMeasureRepository;
+import com.springframework.recipeapp.services.RecipeService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 public class IndexController {
 
+    private final RecipeService recipeService;
+
+    public IndexController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
+
     @RequestMapping({"", "/", "/index"})
-    public String getIndexPage() {
+    public String getIndexPage(Model model) {
+        model.addAttribute("recipes", recipeService.getRecipes());
         return "index";
     }
 }
