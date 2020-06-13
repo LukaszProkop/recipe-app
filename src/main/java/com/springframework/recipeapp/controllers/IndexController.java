35538858ca8ecb87a7dbb9cc37358423db6edeb2
Lukasz Proko <Lukasz.Marek.Prokop@gmail.com>
2020-06-13 23:11:23 +0200

    Added Lombok

--- src/main/java/com/springframework/recipeapp/controllers/IndexController.java
+++ src/main/java/com/springframework/recipeapp/controllers/IndexController.java
@@ -1,16 +1,12 @@
 package com.springframework.recipeapp.controllers;
 
-import com.springframework.recipeapp.domain.Category;
-import com.springframework.recipeapp.domain.UnitOfMeasure;
-import com.springframework.recipeapp.repositories.CategoryRepository;
-import com.springframework.recipeapp.repositories.UnitOfMeasureRepository;
 import com.springframework.recipeapp.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.Optional;
-
+@Slf4j
 @Controller
 public class IndexController {
 
