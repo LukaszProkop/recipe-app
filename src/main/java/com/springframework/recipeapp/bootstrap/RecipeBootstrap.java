b41b2ac056ecd8b82b91bcb24c17a57b47fef369
Lukasz Proko <Lukasz.Marek.Prokop@gmail.com>
2020-06-13 23:13:29 +0200

    Added Lombok

--- src/main/java/com/springframework/recipeapp/bootstrap/RecipeBootstrap.java
+++ src/main/java/com/springframework/recipeapp/bootstrap/RecipeBootstrap.java
@@ -4,6 +4,7 @@ import com.springframework.recipeapp.domain.*;
 import com.springframework.recipeapp.repositories.CategoryRepository;
 import com.springframework.recipeapp.repositories.RecipeRepository;
 import com.springframework.recipeapp.repositories.UnitOfMeasureRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
@@ -13,6 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 @Component
 public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEvent> {
 
