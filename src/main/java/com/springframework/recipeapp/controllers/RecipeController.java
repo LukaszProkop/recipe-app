597232ba59fab5a279e55b20e3e800125f117def
Lukasz Proko <Lukasz.Marek.Prokop@gmail.com>
2020-07-28 00:40:46 +0200

    Added recipe delete

--- src/main/java/com/springframework/recipeapp/controllers/RecipeController.java
+++ src/main/java/com/springframework/recipeapp/controllers/RecipeController.java
@@ -2,10 +2,12 @@ package com.springframework.recipeapp.controllers;
 
 import com.springframework.recipeapp.commands.RecipeCommand;
 import com.springframework.recipeapp.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @Controller
 public class RecipeController {
 
@@ -15,14 +17,14 @@ public class RecipeController {
         this.recipeService = recipeService;
     }
 
-    @RequestMapping("/recipe/{id}/show")
+    @GetMapping("/recipe/{id}/show")
     public String showById(@PathVariable String id, Model model){
 
         model.addAttribute("recipe", recipeService.findById(Long.valueOf(id)));
         return "recipe/show";
     }
 
-    @RequestMapping("recipe/new")
+    @GetMapping("recipe/new")
     public String newRecipe(Model model){
         model.addAttribute("recipe", new RecipeCommand());
 
@@ -40,5 +42,13 @@ public class RecipeController {
         RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
 
         return "redirect:/recipe/" + savedCommand.getId() + "/show";
+    }
+
+    @GetMapping("recipe/{id}/delete")
+    public String deleteById(@PathVariable String id){
+        log.debug("Deleted id: " + id);
+        recipeService.deleteById(Long.valueOf(id));
+
+        return "redirect:/";
     }
 }
