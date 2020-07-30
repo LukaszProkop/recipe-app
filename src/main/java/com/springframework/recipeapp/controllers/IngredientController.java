c523cd330087b7e64630799fff7447a392597f78
Lukasz Proko <Lukasz.Marek.Prokop@gmail.com>
2020-07-31 00:24:43 +0200

    Show Ingredient

--- src/main/java/com/springframework/recipeapp/controllers/IngredientController.java
+++ src/main/java/com/springframework/recipeapp/controllers/IngredientController.java
@@ -1,5 +1,6 @@
 package com.springframework.recipeapp.controllers;
 
+import com.springframework.recipeapp.services.IngredientService;
 import com.springframework.recipeapp.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -11,17 +12,28 @@ import org.springframework.web.bind.annotation.PathVariable;
 @Controller
 public class IngredientController {
     private final RecipeService recipeService;
+    private final IngredientService ingredientService;
 
-    public IngredientController(RecipeService recipeService) {
+    public IngredientController(RecipeService recipeService, IngredientService ingredientService) {
         this.recipeService = recipeService;
+        this.ingredientService = ingredientService;
     }
 
     @GetMapping("/recipe/{recipeId}/ingredients")
-    public String listIngredients(@PathVariable String recipeId, Model model){
+    public String listIngredients(@PathVariable String recipeId, Model model) {
         log.debug("Getting ingredient list for recipe id: " + recipeId);
 
         model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(recipeId)));
 
         return "recipe/ingredient/list";
+    }
+
+    @GetMapping("/recipe/{recipeId}/ingredient/{ingredientId}/show")
+    public String showRecipeIngredient(@PathVariable String recipeId, @PathVariable String ingredientId, Model model) {
+
+        model.addAttribute("ingredient", ingredientService
+                .findByRecipeIdAndIngredientId(Long.valueOf(recipeId), Long.valueOf(ingredientId)));
+
+        return "recipe/ingredient/show";
     }
 }
---
+++ src/main/java/com/springframework/recipeapp/services/IngredientServiceImpl.java
@@ -0,0 +1,45 @@
+package com.springframework.recipeapp.services;
+
+import com.springframework.recipeapp.commands.IngredientCommand;
+import com.springframework.recipeapp.converters.IngredientToIngredientCommand;
+import com.springframework.recipeapp.domain.Recipe;
+import com.springframework.recipeapp.repositories.RecipeRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Slf4j
+@Service
+public class IngredientServiceImpl implements IngredientService {
+
+    private final IngredientToIngredientCommand ingredientToIngredientCommand;
+    private final RecipeRepository recipeRepository;
+
+    public IngredientServiceImpl(IngredientToIngredientCommand ingredientToIngredientCommand, RecipeRepository recipeRepository) {
+        this.ingredientToIngredientCommand = ingredientToIngredientCommand;
+        this.recipeRepository = recipeRepository;
+    }
+
+    @Override
+    public IngredientCommand findByRecipeIdAndIngredientId(Long recipeId, Long ingredientId) {
+        Optional<Recipe> recipeOptional = recipeRepository.findById(recipeId);
+
+        if (!recipeOptional.isPresent()){
+            log.error("recipe is not found. Id: " + recipeId);
+        }
+
+        Recipe recipe = recipeOptional.get();
+
+        Optional<IngredientCommand> ingredientCommandOptional = recipe.getIngredients().stream()
+                .filter(ingredient -> ingredient.getId().equals(ingredientId))
+                .map(ingredient -> ingredientToIngredientCommand.convert(ingredient))
+                .findFirst();
+
+        if (ingredientCommandOptional.isPresent()){
+            log.error("Ingredient Id not found: " + ingredientId);
+        }
+
+        return ingredientCommandOptional.get();
+    }
+}
