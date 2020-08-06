037f91dd68cabe655393ae09ce8b1a02c16a9246
Lukasz Proko <Lukasz.Marek.Prokop@gmail.com>
2020-08-06 17:51:42 +0200

    Handle NumberFormatException

--- src/main/java/com/springframework/recipeapp/controllers/RecipeController.java
+++ src/main/java/com/springframework/recipeapp/controllers/RecipeController.java
@@ -23,7 +23,7 @@ public class RecipeController {
     @GetMapping("/recipe/{id}/show")
     public String showById(@PathVariable String id, Model model){
 
-        model.addAttribute("recipe", recipeService.findById(Long.valueOf(id)));
+        model.addAttribute("recipe", recipeService.findById(new Long(id)));
         return "recipe/show";
     }
 
@@ -64,6 +64,20 @@ public class RecipeController {
         ModelAndView modelAndView = new ModelAndView();
 
         modelAndView.setViewName("404error");
+        modelAndView.addObject("exception", exception);
+
+        return modelAndView;
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(NumberFormatException.class)
+    public ModelAndView handleNumberFormat(Exception exception){
+        log.error("Handling number format exception");
+        log.error(exception.getMessage());
+
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.setViewName("400error");
         modelAndView.addObject("exception", exception);
 
         return modelAndView;
--- src/test/java/com/springframework/recipeapp/controllers/RecipeControllerTest.java
+++ src/test/java/com/springframework/recipeapp/controllers/RecipeControllerTest.java
@@ -53,14 +53,18 @@ public class RecipeControllerTest {
 
     @Test
     public void testRecipeNotFound() throws Exception{
-        Recipe recipe = new Recipe();
-        recipe.setId(1L);
-
         when(recipeService.findById(anyLong())).thenThrow(NotFoundException.class);
 
         mockMvc.perform(get("/recipe/1/show"))
                 .andExpect(status().isNotFound())
                 .andExpect(view().name("404error"));
+    }
+
+    @Test
+    public void testRecipeNumberFormatException() throws Exception{
+        mockMvc.perform(get("/recipe/sfg/show"))
+                .andExpect(status().isBadRequest())
+                .andExpect(view().name("400error"));
     }
 
     @Test
