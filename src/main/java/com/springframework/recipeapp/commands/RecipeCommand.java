90f1e0075c955fd91e1a9db43e27969e82f213aa
Lukasz Proko <Lukasz.Marek.Prokop@gmail.com>
2020-08-06 23:06:52 +0200

    Added Validation

--- src/main/java/com/springframework/recipeapp/commands/RecipeCommand.java
+++ src/main/java/com/springframework/recipeapp/commands/RecipeCommand.java
@@ -1,13 +1,15 @@
 package com.springframework.recipeapp.commands;
 
-import com.springframework.recipeapp.domain.Category;
 import com.springframework.recipeapp.domain.Difficulty;
-import com.springframework.recipeapp.domain.Ingredient;
-import com.springframework.recipeapp.domain.Notes;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.validator.constraints.URL;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -16,13 +18,31 @@ import java.util.Set;
 @NoArgsConstructor
 public class RecipeCommand {
     private Long id;
+
+    @NotBlank()
+    @Size(min = 3, max = 255)
     private String description;
+
+    @Min(1)
+    @Max(999)
     private Integer prepTime;
+
+    @Min(1)
+    @Max(999)
     private Integer cookTime;
+
+    @Min(1)
+    @Max(999)
     private Integer servings;
+
     private String source;
+
+    @URL
     private String url;
+
+    @NotBlank
     private String directions;
+
     private Set<IngredientCommand> ingredients = new HashSet<>();
     private Byte[] image;
     private Difficulty difficulty;
